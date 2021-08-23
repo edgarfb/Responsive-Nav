40d92585aabe89cc22e6db1a81c6c4006ab39ac7
OtabekAbdurakhmonov <77769473+OtabekAbdurakhmonov@users.noreply.github.com>
2021-08-23 21:37:52 +0900

    changes

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
+ var dark = false;
+console.log(false);
+
+var icon = document.getElementById("icon");
+
+function toggle () {
+
+  if(dark) {
+    document.body.className = "";
+    dark = false;
+    console.log(dark);
+  }else{
+    document.body.className = "dark-theme";
+    dark = true;
+    console.log(true);
+  }
+
+}
+//   dark==true ? function(){
+//     document.body.className = "";
+//     dark = false;
+//     console.log(dark);
+//   }:function(){
+//     document.body.className = "dark-theme";
+//     dark = true;
+//     console.log(true);
+//   }
+// }
+
+
+
+  // var dark = false;
+  // icon.onclick = function(){
+  //   dark ? function(){
+  //     document.body.className = "";
+  //     dark = false;
+  //     console.log(dark);
+  //   }:function(){
+  //     document.body.className = "dark-theme";
+  //     dark = true;
+  //     console.log(true);
+  //   }
+  // };
+
 
 
